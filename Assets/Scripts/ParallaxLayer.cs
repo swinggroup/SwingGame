b31e9a3bac083527ac95571bf593bdc419a33f5d
d q <frogsonatree@gmail.com>
2023-10-15 08:19:27 -0700

    Add reflections for gates and temple in lake, add parallax layers, start snake animation

--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -6,17 +6,31 @@ public class ParallaxLayer : MonoBehaviour
 {
     public PlayerController playerController;
     private float xCenter = -28f;
+    private float startXPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startXPos = this.transform.position.x; 
     }
 
     // Update is called once per frame
     void Update()
     {
         // standard parallax
-        this.transform.position = new Vector3(-0.02f * (playerController.transform.position.x - (xCenter)) + (xCenter), this.transform.position.y, this.transform.position.z);
+        if (gameObject.CompareTag("Parallax1"))
+        {
+            this.transform.position = new Vector3(-0.08f * (playerController.transform.position.x - (startXPos)) + (startXPos), this.transform.position.y, this.transform.position.z);
+        } else if (gameObject.CompareTag("Parallax2"))
+        {
+            this.transform.position = new Vector3(-0.04f * (playerController.transform.position.x - (startXPos)) + (startXPos), this.transform.position.y, this.transform.position.z);
+        } else if (gameObject.CompareTag("Parallax3"))
+        {
+            this.transform.position = new Vector3(-0.025f * (playerController.transform.position.x - (startXPos)) + (startXPos), this.transform.position.y, this.transform.position.z);
+        } else
+        {
+            Debug.LogError("no parallax tag on this object: " + gameObject);
+            return;
+        }
 
         // parallax, but centered on the player
         // this.transform.position = new Vector3(-0.15f * (playerController.transform.position.x - (xCenter)) + (playerController.transform.position.x), this.transform.position.y, this.transform.position.z);
---
+++ Assets/Scripts/ParallaxScrollLayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrollLayer : MonoBehaviour
+{
+    List<Transform> children;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform child in transform)
+        {
+            child.position += new Vector3(1f * Time.deltaTime, 0, 0);
+            if (child.position.x > 61)
+            {
+                child.position -= new Vector3(3 * 62.5f, 0, 0);
+            }
+        }
+    }
+}
