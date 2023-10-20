d65b30943ba0cc637d3ca1a32d03b0669eb1b89a
d q <frogsonatree@gmail.com>
2023-10-20 16:32:16 -0700

    add animated unhookable tile, add background brick tiles, modify parallax script to move objects in y direction as well

--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -7,10 +7,12 @@ public class ParallaxLayer : MonoBehaviour
     public PlayerController playerController;
     private float xCenter = -28f;
     private float startXPos;
+    private float startYPos;
     // Start is called before the first frame update
     void Start()
     {
         startXPos = this.transform.position.x; 
+        startYPos = this.transform.position.y; 
     }
 
     // Update is called once per frame
@@ -19,13 +21,13 @@ public class ParallaxLayer : MonoBehaviour
         // standard parallax
         if (gameObject.CompareTag("Parallax1"))
         {
-            this.transform.position = new Vector3(-0.08f * (playerController.transform.position.x - (startXPos)) + (startXPos), this.transform.position.y, this.transform.position.z);
+            this.transform.position = new Vector3(-0.08f * (playerController.transform.position.x - (startXPos)) + (startXPos), -0.08f * (playerController.transform.position.y - (startYPos)) + (startYPos), this.transform.position.z);
         } else if (gameObject.CompareTag("Parallax2"))
         {
-            this.transform.position = new Vector3(-0.04f * (playerController.transform.position.x - (startXPos)) + (startXPos), this.transform.position.y, this.transform.position.z);
+            this.transform.position = new Vector3(-0.04f * (playerController.transform.position.x - (startXPos)) + (startXPos), -0.04f * (playerController.transform.position.y - (startYPos)) + (startYPos), this.transform.position.z);
         } else if (gameObject.CompareTag("Parallax3"))
         {
-            this.transform.position = new Vector3(-0.025f * (playerController.transform.position.x - (startXPos)) + (startXPos), this.transform.position.y, this.transform.position.z);
+            this.transform.position = new Vector3(-0.025f * (playerController.transform.position.x - (startXPos)) + (startXPos), -0.025f * (playerController.transform.position.y - (startYPos)) + (startYPos), this.transform.position.z);
         } else
         {
             Debug.LogError("no parallax tag on this object: " + gameObject);
