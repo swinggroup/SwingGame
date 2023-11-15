a7b7ab0f362bf45b243676b8aa3f63458a11a739
d q <frogsonatree@gmail.com>
2023-11-15 00:56:15 -0800

    Parallax Buddhas WIP

--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -4,41 +4,90 @@ using UnityEngine;
 
 public class ParallaxLayer : MonoBehaviour
 {
-    public PlayerController playerController;
     private float startXPos;
     private float startYPos;
-    private float playerXStart;
-    private float playerYStart;
+    readonly private float cameraXStart = -81.5625f;
+    readonly private float cameraYStart = -60f;
+
+    readonly private float PARALLAX_1 = 0.13f;
+    readonly private float PARALLAX_2 = 0.18f;
+    readonly private float PARALLAX_3 = 0.23f;
+    readonly private float PARALLAX_4 = 0.28f;
+    readonly private float PARALLAX_5 = 0.33f;
+    readonly private float PARALLAX_6 = 0.38f;
+    readonly private float PARALLAX_7 = 0.43f;
+    readonly private float PARALLAX_8 = 0.48f;
+    readonly private float PARALLAX_9 = 0.53f;
+    readonly private float PARALLAX_10 = 0.58f;
+    readonly private float PARALLAX_11 = 0.63f;
+    readonly private float PARALLAX_12 = 0.68f;
+    readonly private float PARALLAX_13 = 0.73f;
+    readonly private float PARALLAX_14 = 0.78f;
+    readonly private float PARALLAX_15 = 0.83f;
+
     // Start is called before the first frame update
     void Start()
     {
         startXPos = this.transform.position.x; 
         startYPos = this.transform.position.y;
-        playerXStart = playerController.transform.position.x;
-        playerYStart = playerController.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float parallax = 0;
         // standard parallax
-        if (gameObject.CompareTag("Parallax1"))
+        switch (gameObject.tag)
         {
-            this.transform.position = new Vector3(-0.08f * (playerController.transform.position.x - (playerXStart)) + (startXPos), -0.08f * (playerController.transform.position.y - (playerYStart)) + (startYPos), this.transform.position.z);
-        } else if (gameObject.CompareTag("Parallax2"))
-        {
-            this.transform.position = new Vector3(-0.04f * (playerController.transform.position.x - (playerXStart)) + (startXPos), -0.04f * (playerController.transform.position.y - (playerYStart)) + (startYPos), this.transform.position.z);
-        } else if (gameObject.CompareTag("Parallax3"))
-        {
-            // this.transform.position = new Vector3(-0.025f * (playerController.transform.position.x - (playerXStart)) + (startXPos), -0.025f * (playerController.transform.position.y - (playerYStart)) + (startYPos), this.transform.position.z);
-            this.transform.position = new Vector3(((250f/300f) * playerController.transform.position.x - (playerXStart)) + (startXPos), ((250f/300f) * playerController.transform.position.y - (playerYStart)) + (startYPos), this.transform.position.z);
-        } else
-        {
-            Debug.LogError("no parallax tag on this object: " + gameObject);
-            return;
+            case "Parallax1":
+                parallax = PARALLAX_1;
+                break;
+            case "Parallax2":
+                parallax = PARALLAX_2;
+                break;
+            case "Parallax3":
+                parallax = PARALLAX_3;
+                break;
+            case "Parallax4":
+                parallax = PARALLAX_4;
+                break;
+            case "Parallax5":
+                parallax = PARALLAX_5;
+                break;
+            case "Parallax6":
+                parallax = PARALLAX_6;
+                break;
+            case "Parallax7":
+                parallax = PARALLAX_7;
+                break;
+            case "Parallax8":
+                parallax = PARALLAX_8;
+                break;
+            case "Parallax9":
+                parallax = PARALLAX_9;
+                break;
+            case "Parallax10":
+                parallax = PARALLAX_10;
+                break;
+            case "Parallax11":
+                parallax = PARALLAX_11;
+                break;
+            case "Parallax12":
+                parallax = PARALLAX_12;
+                break;
+            case "Parallax13":
+                parallax = PARALLAX_13;
+                break;
+            case "Parallax14":
+                parallax = PARALLAX_14;
+                break;
+            case "Parallax15":
+                parallax = PARALLAX_15;
+                break;
+            default:
+                Debug.LogError("no parallax tag on object" + gameObject);
+                return;
         }
-
-        // parallax, but centered on the player
-        // this.transform.position = new Vector3(-0.15f * (playerController.transform.position.x - (xCenter)) + (playerController.transform.position.x), this.transform.position.y, this.transform.position.z);
+        this.transform.position = new Vector3((parallax * (Camera.main.transform.position.x - cameraXStart)) + (startXPos), (parallax * (Camera.main.transform.position.y - cameraYStart)) + (startYPos), this.transform.position.z);
     }
 }
